b314617352ff6a5765cf2a4135ce34b61f8cbb8e
martin daniel <martin.daniel@esme.fr>
2022-04-10 13:32:41 +0200

    j'ai mis à jour mon code avec le principal

--- src/request_api.py
+++ src/request_api.py
@@ -1,8 +1,49 @@
 import requests
-
-url = "https://api.twitter.com/2/tweets/search/recent?query=pomme -is:retweet"
+import json
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+url = "https://api.twitter.com/2/tweets/search/recent?query=concombre -is:retweet"
 headers = {
     "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAACNWaQEAAAAAQaaBxfOjJCX1m3H%2BF0qrSGFIKPc%3DQxdEvIXQi90IT32aQ8JDcOmxgnp34idYC5H712BzQfybgyRv9r"
 }
 
-response = requests.get(url, headers = headers)
+# response = requests.get(url, headers=headers)
+# répertoire=response.json()
+#
+# with open("data_file.json", "w") as write_file:
+#     json.dump(répertoire, write_file,indent=1,)
+#
+# with open("data_file.json", "r") as read_file:
+#     RecTweet = json.load(read_file)
+#
+# for key in RecTweet:
+# 	print(json.dumps(RecTweet, indent=4,ensure_ascii=False))
+#
+
+comptage = "https://api.twitter.com/2/tweets/counts/recent?query=concombre&granularity=day"
+Compt = requests.get(comptage, headers=headers)
+CompTweet=Compt.json()
+#print(json.dumps(CompTweet, indent=4,ensure_ascii=False))
+
+# for count in CompTweet["data"]:
+#     print(count)
+
+
+df = pd.DataFrame(CompTweet["data"])
+Jour = ['-7','-6','-5','-4','-3','-2','-1','-0']
+df['Jour'] = Jour
+# print(df.head())
+
+with plt.style.context('dark_background') :
+    plt.plot(df["Jour"], df["tweet_count"], color='blue')
+    plt.title("Nombre de Tweet sur les 7 derniers jours contenant le mot : concombre")
+    plt.xlabel("Jour précédent la recherche")
+    plt.ylabel("Nombre de Tweet")
+    plt.show()
+
+plt.hist(df['tweet_count'])
+plt.title('nb de tweet')
+plt.xlabel('jour')
+plt.ylabel('Nombre de tweet')
+plt.show()
