5e6c0e1641c2dd5c5d89ac8b1b40dcae66a24b38
martin daniel <martin.daniel@esme.fr>
2022-05-08 13:38:40 +0200

    new update

--- src/main.py
+++ src/main.py
@@ -2,9 +2,15 @@ from tweet_fetcher import TweetFetcher
 from tweet_counter import TweetCounter
 
 BASE_URL = "https://api.twitter.com/2/tweets/"
-
+next =""
 fetcher = TweetFetcher(BASE_URL)
 result = fetcher.get_tweet("macron",True)
+if result.key("next_token") =! None:
+
+    next = next +"&"+ result['meta']['next_token']
+    next_fetcher=TweetFetcher(BASE_URL+next)
+    next_result = next_fetcher.get_tweet("macron", True)
+    fetcher.export(next_result, "macron")
 
 fetcher.export(result,"macron")
 
