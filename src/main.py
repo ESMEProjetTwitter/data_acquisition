44cc0e3e60bafae014fe05badbac5a01a02a82ba
Nathan Thury <nathan95230@gmail.com>
2022-05-12 12:07:13 +0200

    Add Tweets counter and export to GCP

--- src/main.py
+++ src/main.py
@@ -2,14 +2,19 @@ from tweet_fetcher import TweetFetcher
 from tweet_counter import TweetCounter
 
 BASE_URL = "https://api.twitter.com/2/tweets/"
-next = ""
 
-fetcher = TweetFetcher(BASE_URL)
 keyword = "macron"
-result = fetcher.get_tweet(keyword,True)
-print(str(len(result)) + " tweets were retrieved")
 
-fetcher.export(result, keyword)
+next = ""
+fetcher = TweetFetcher(BASE_URL)
+result1 = fetcher.get_tweet(keyword,True)
+print(str(len(result1)) + " tweets were retrieved")
+fetcher.export(result1, keyword)
 print(keyword + " tweets have been written to GCS")
+
+counter = TweetCounter(BASE_URL)
+result2 = counter.count_tweet(keyword)
+counter.export(result2, keyword)
+print(keyword + " counts have been written to GCS")
 
 
--- src/tweet_counter.py
+++ src/tweet_counter.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from google.cloud import storage
+from datetime import date
 
 class TweetCounter:
 
@@ -9,13 +11,14 @@ class TweetCounter:
         self.url = base_url
 
     def make_url(self, keyword):
-        url2 = self.url + "counts/recent?query=" + keyword
+        url2 = self.url + "counts/recent?query=" + keyword + "&granularity=hour"
         #action sur la granularité ?
         return url2
 
     def count_tweet(self, keyword):
         url2 = self.make_url(keyword)
         result = self.query(url2)
+        return result
 
     def query(self, url2):
         headers = {
@@ -24,6 +27,18 @@ class TweetCounter:
         response = requests.get(url2, headers=headers)
         return response.json()
 
-    def export(self, data):
-        print("Export counts to Google Cloud Storage")
+    def make_filename(self, keyword):
+        today = date.today()
+        today = today.strftime("%Y-%m-%d")
+        html_name = "{}/{}/tweets_count.json".format(today, keyword)  # once per day on Gcloud -->date/keyword/tweets
+        return html_name
 
+    def export(self, data, keyword):
+        data_json = json.dumps(data, indent=4, ensure_ascii=False)
+        print(data)
+        client = storage.Client()
+        bucket = client.get_bucket('tweets-project-esme')  # bucket name -->unique name
+        filename = self.make_filename(keyword)
+        blob = bucket.blob("tweets/" + filename)
+        blob.upload_from_string(data_json)
+        print("Export counts to Google Cloud Storage")
