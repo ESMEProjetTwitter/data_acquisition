65e83ee1a2fc48baa47194102d07a59eb8490775
Nathan Thury <nathan95230@gmail.com>
2022-05-12 12:43:25 +0200

    Add test filename

--- test/test_twitter_counter.py
+++ test/test_twitter_counter.py
@@ -1,10 +1,17 @@
 from src.tweet_counter import TweetCounter
+from datetime import date
 
 BASE_URL = "https://api.twitter.com/2/tweets/"
+counter = TweetCounter(BASE_URL)
 
 def test_make_url_count():
-    counter = TweetCounter(BASE_URL)
     url = counter.make_url("macron")
-    expected_url = "https://api.twitter.com/2/tweets/counts/recent?query=macron"
+    expected_url = "https://api.twitter.com/2/tweets/counts/recent?query=macron&granularity=hour"
     assert url == expected_url
+
+def test_make_filename():
+    filename = counter.make_filename("macron")
+    today = date.today().strftime("%Y-%m-%d")
+    expected_filename = today + "/macron/tweets_count.json"
+    assert filename == expected_filename
 
