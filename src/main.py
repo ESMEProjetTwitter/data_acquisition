676a0fd3a701924f8a24e3c733fcdfdf60c5d06c
Yinshangyi <jl.dejaeger@gmail.com>
2022-05-25 20:56:54 +0200

    Analyzing bug

--- src/main.py
+++ src/main.py
@@ -1,19 +1,27 @@
+import flask
+
 from tweet_counter import TweetCounter
 from tweet_fetcher import TweetFetcher
 
 BASE_URL = "https://api.twitter.com/2/tweets/"
 
-keywords = ["macron", "zemmour", "le pen", "mélenchon"]
+keywords = ["le pen"]
 
 fetcher = TweetFetcher(BASE_URL)
 counter = TweetCounter(BASE_URL)
 
-for keyword in keywords:
-    tweets = fetcher.get_tweet(keyword, True)
-    duplicates = fetcher.count_duplicates(tweets)
-    print("{} tweets have been fetched with {} duplicates".format(len(tweets), duplicates))
-    fetcher.export(tweets, keyword)
 
-    counts = counter.count_tweet(keyword)
-    counter.export(counts, keyword)
-    print(keyword + " counts have been written to GCS")
+def run_tweets_acquisition(request):
+    for keyword in keywords:
+        tweets = fetcher.get_tweet(keyword, True, 100)
+        duplicates = fetcher.count_duplicates(tweets)
+        print("{} tweets have been fetched with {} duplicates".format(len(tweets), duplicates))
+        fetcher.export(tweets, keyword)
+
+        counts = counter.count_tweet(keyword)
+        counter.export(counts, keyword)
+        print(keyword + " counts have been written to GCS")
+    flask.Response(status=200)
+
+
+run_tweets_acquisition(None)
--- src/tweet_fetcher.py
+++ src/tweet_fetcher.py
@@ -1,19 +1,19 @@
-import requests
 import json
-from google.cloud import storage
 from datetime import date
+
+import requests
+from google.cloud import storage
 
 
 class TweetFetcher:
-   
     TOKEN = "AAAAAAAAAAAAAAAAAAAAACNWaQEAAAAAQaaBxfOjJCX1m3H%2BF0qrSGFIKPc%3DQxdEvIXQi90IT32aQ8JDcOmxgnp34idYC5H712BzQfybgyRv9r"
 
     def __init__(self, base_url):
         self.url = base_url
 
     def make_url(self, keyword, retweet):
-        url = self.url + "search/recent?query=" + keyword 
-        if retweet == True :
+        url = self.url + "search/recent?query=" + keyword
+        if retweet == True:
             url = url + " -is:retweet"
         return url
 
@@ -21,9 +21,9 @@ class TweetFetcher:
         url = url + "&next_token=" + token
         return url
 
-    def get_tweet(self, keyword, retweet):
+    def get_tweet(self, keyword, retweet, nb_page):
         url = self.make_url(keyword, retweet)
-        result = self.query(url)
+        result = self.query(url, nb_page)
         return result
 
     def flatten(self, results2d):
@@ -33,34 +33,35 @@ class TweetFetcher:
                 flat_list.append(item)
         return flat_list
 
-    def query(self, url):
+    def query(self, url, nb_page):
         headers = {
-        "Authorization": "Bearer " + TweetFetcher.TOKEN
+            "Authorization": "Bearer " + TweetFetcher.TOKEN
         }
         next_token = None
         all_tweets = []
         response = requests.get(url, headers=headers)
         all_tweets.append(response.json()["data"])
+        # if meta.next_token exists -> next_token = response.json()['meta']['next_token']
         next_token = response.json()['meta']['next_token']
         # Make the query for page 2 to 100 of the API
-        for page_num in range(1, 10):
-            if next_token is not None:
+        for page_num in range(1, nb_page):
+            if next_token is not None: # not safe!
                 url_with_next_token = self.add_next_token(url, next_token)
                 response = requests.get(url_with_next_token, headers=headers)
                 next_token = response.json()['meta']['next_token']
                 all_tweets.append(response.json()["data"])
         return self.flatten(all_tweets)
 
-    def make_filename(self,keyword):
+    def make_filename(self, keyword):
         today = date.today()
         today = today.strftime("%Y-%m-%d")
-        html_name = "{}/{}/tweets.json".format(today,keyword)  #once per day on Gcloud -->date/keyword/tweets
+        html_name = "{}/{}/tweets.json".format(today, keyword)  # once per day on Gcloud -->date/keyword/tweets
         return html_name
 
-    def export(self,data,keyword):
-        data_json = json.dumps(data, indent=4,ensure_ascii=False)
+    def export(self, data, keyword):
+        data_json = json.dumps(data, indent=4, ensure_ascii=False)
         client = storage.Client()
-        bucket = client.get_bucket('tweets-project-esme') # bucket name -->unique name
+        bucket = client.get_bucket('tweets-project-esme')  # bucket name -->unique name
         filename = self.make_filename(keyword)
         blob = bucket.blob("tweets/" + filename)
         blob.upload_from_string(data_json)
